ab7cda646ccdb9635c119f5f992a5c18b9a0976d
Florian Engels <florianagario2@gmail.com>
2024-02-20 19:21:07 +0100

    rewrote hud manager since it was broken before

--- src/hud.ts
+++ src/hud.ts
@@ -6,158 +6,60 @@ import { VectorFunctions } from './staticScripts/vectorFunctions';
 import { Logger } from './staticScripts/Logger';
 import { GlobalVars } from './globalVars';
 import { TickFunctions } from './staticScripts/tickFunctions';
+import { LinkedList } from 'dataTypes/linkedList';
 //import { listenerCount } from 'gulp';
 
-export { ActionbarMessage, HudManager}
-
-class ActionbarMessage{
-    /**
-     *  @type {Player} 
-    */
-        player
-    /**
-     * @type {String}
-     */
-    message 
-
-    /**
-     * @type {number}
-     */
-    lifetime
-
-
-    /**
-     * 
-     * @param {Player} player 
-     * @param {String} message 
-     * @param {number} lifetime 
-     */
-    constructor(player, message, lifetime){
-        this.player = player
-        this.message = message
-        this.lifetime = lifetime
-    }
-}
-
-interface IActionbarMessage{
+export interface IActionbarMessage{
     player: Player
     message: String
+    /**How many ticks to show this message */
     lifetime: number
 }
 
+let actionbarMessages = new LinkedList<IActionbarMessage>();
 
-class HudManager{
-    /**
-     * @type {IActionbarMessage[]}
-     */
-    static actionbarMessages = []
+TickFunctions.addFunction(() => tick(), 1)
 
-    static init(){
-        TickFunctions.addFunction(() => this.tick(), 0)
-    }
+let centerFiller = 0;
+const tick = () => {
+    for(const player of GlobalVars.players){
+        //world.sendMessage(`Showing ${player} with ${actionbarMessages.size} messages`)
+        let combiendMessage = ""
+        let maxLength = 0 
 
-    static tick(){
-        for(const player of GlobalVars.players){
-            let combiendMessage = ""
-            let maxLength = 0 
-            for(let i = 0; i < this.actionbarMessages.length; i++){
-                if(this.actionbarMessages[i].message.length > maxLength){
-                    maxLength = this.actionbarMessages[i].message.length
-                }
+        actionbarMessages.forEach((actionbarMessage, index) => {
+            if(!(actionbarMessage.player.name == player.name)){
+                return;
             }
-            for(let i = 0; i < this.actionbarMessages.length; i++){
-                //world.sendMessage(`YO ${this.actionbarMessages[i].player.name} = ${ player.name}`)
-                if(this.actionbarMessages[i].player.name == player.name){
-                    
-                    let addSpace = Math.ceil((maxLength - this.actionbarMessages[i].message.length) / 2)
-                    //world.sendMessage(""+maxLength + " T: " + this.actionbarMessages[i].message.length + " A: " + addSpace)
-                    for(let i = 0; i < addSpace; i++){
-                        combiendMessage = `${combiendMessage} `
-                    }
-                    combiendMessage = `${combiendMessage}${this.actionbarMessages[i].message}\n`
-                    this.actionbarMessages[i].lifetime = this.actionbarMessages[i].lifetime - 1
-                    if(this.actionbarMessages[i].lifetime < 1){
-                        this.actionbarMessages = this.actionbarMessages.slice(0, i).concat(this.actionbarMessages.slice(i+1))
-                    
-                        
-                    }
-                }
-                    
+            
+            centerFiller = Math.ceil((maxLength - actionbarMessage.message.length) / 2)
+
+                //world.sendMessage(""+maxLength + " T: " + actionbarMessages[i].message.length + " A: " + addSpace)
+            for(let i = 0; i < centerFiller; i++){
+                combiendMessage = `${combiendMessage} `
             }
-            player.onScreenDisplay.setActionBar(combiendMessage)
-        }
-        
+
+            combiendMessage = `${combiendMessage}${actionbarMessage.message}\n`
+            actionbarMessage.lifetime--;
+            if(actionbarMessage.lifetime < 1){
+                actionbarMessages.deleteNodeByIndex(index)
+            }
+            
+        })
+        player.onScreenDisplay.setActionBar(combiendMessage)
     }
+}      
+    
     /**
      * 
      * @param {actionbarMessage} actionbarMessage
      */
-    static addActionbarMessage(actionbarMessage){
-        this.actionbarMessages = this.actionbarMessages.concat([actionbarMessage])
-    }
-    /**
-     * 
-     * @param {Player} player
-     * @param {String} message
-     * @param {number} lifetime For how many ticks to display
-     */
-    //addActionbarMessageScratch(player, message, lifetime){
-     //   this.actionbarMessages.concat([new ActionbarMessage(player, message, lifetime)])
-    //}
+export const addActionbarMessage = (actionbarMessage : IActionbarMessage) => {
+    actionbarMessages.append(actionbarMessage)
 }
 
-HudManager.init()
 for(const player of world.getAllPlayers()){
-    HudManager.addActionbarMessage(new ActionbarMessage(player, `§a§lHud initalised!`, 100))
+    world.sendMessage(`§a§lHud initalised!`)
+    addActionbarMessage({player: player, message:`§a§lHud initalised!`, lifetime: 100})
 }
 
-class Timer{
-
-    #queuePenalty = 0;
-
-    init(){
-        system.runInterval(async () => {
-            for(const player of GlobalVars.players){
-                /*world.getDimension("overworld").spawnParticle("minecraft:totem_particle", VectorFunctions.addVector(player.location, VectorFunctions.multiplyVector(VectorFunctions.getForwardVectorFromRotationXY(player.getRotation().x, player.getRotation().y), 3)))
-                if(!CollisionFunctions.insideBox(player.location, VectorFunctions.subtractVector(parkour.getCp(0), {x:2, y:2, z:2}), VectorFunctions.addVector(oldParkour.getCp(0), {x:2, y:2, z:2}))){
-                    
-                    if(this.#queuePenalty != 0) {
-                        await ScoreboardFunctions.setScore(player, "ticks", ScoreboardFunctions.getScore(player, "ticks") + 1 + this.#queuePenalty)
-                        this.#queuePenalty = 0
-                    }
-                    else{
-                        await ScoreboardFunctions.setScore(player, "ticks", ScoreboardFunctions.getScore(player, "ticks") + 1)
-                    }
-                    
-         
-                }
-                else{
-                    ScoreboardFunctions.setScore(player, "ticks", 0)
-                    ScoreboardFunctions.setScore(player, "deaths", 0)
-                }
-                */
-                let ticks = ScoreboardFunctions.getScore(player, "ticks")
-                let seconds = ticks / 20;
-                let hours = Math.floor(seconds / 60 / 60)
-                let minutes = Math.floor(seconds / 60 % 60)
-                let roundSeconds = Math.floor(seconds % 60)
-                HudManager.addActionbarMessage( new ActionbarMessage(
-                    player,
-                    `§a${hours}:${minutes}:${roundSeconds}:${((seconds%1).toFixed(2)).toString().slice(2)} | Ticks: ${ScoreboardFunctions.getScore(player, "ticks")} | Deaths: ${ScoreboardFunctions.getScore(player, "deaths")}`,
-                    1
-                ))
-                //player.onScreenDisplay.setActionBar(`§a ${hours}:${minutes}:${roundSeconds}:${((seconds%1).toFixed(2)).toString().slice(2)} | Ticks: ${ScoreboardFunctions.getScore(player, "ticks")} | At ${20}TPS`)
-            }
-        })
-    }
-
-    /**
-     * 
-     * @param {*} amount 
-     */
-    addPenalty(amount){
-        this.#queuePenalty = amount
-    }
-}
-
-//let timer = new Timer 
